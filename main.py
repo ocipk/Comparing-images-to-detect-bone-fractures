5d76735b5f991419432c684f87f9d84e20294f83
ocipk <aliasiri018@gmail.com>
2025-11-21 01:39:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import cv2 
+PH1 = cv2.imread(r'c:\Users\tooln\Desktop\Myfirstproject\basic.jpg')
+PH2 = cv2.imread(r'C:\Users\tooln\Desktop\Myfirstproject\images\tow.jpg')
+
+if PH1.shape == PH2.shape :
+    print('The image have same size and channels')
+    diff = cv2.subtract(PH1,PH2)
+    b ,g ,r  = cv2.split(diff)
+    if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) and cv2.countNonZero(r):
+        print('The images are completly Equal')
+else:
+    print('the images are not Equal')
